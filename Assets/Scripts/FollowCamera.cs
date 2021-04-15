8ffe38ce01b24f3a3346e7a180a6d1854887dee7
ivan lashin <iw.lasgin2011@gmail.com>
2021-04-15 22:20:54 +0300

    add moving again

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RPG.Core
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform Target;
+
+        private void LateUpdate()
+        {
+            transform.position = Target.position;
+        }
+    } 
+}
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace TestLavaProject.Core
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private NavMeshAgent _navMeshAgent;
+
+        [SerializeField] private float MaxSpeed = 10f;
+
+        private void Update()
+        {
+            TryMove();
+        }
+
+        private void TryMove()
+        {
+            if (!Physics.Raycast(GetMouseRay(), out var hit)) {return;}
+            
+            if (Input.GetMouseButton(0))
+            {
+                MoveTo(hit.point, 1f);
+            }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        
+        private void MoveTo(Vector3 destination, float speedFraction)
+        {
+            _navMeshAgent.destination = destination;
+            _navMeshAgent.speed = MaxSpeed * Mathf.Clamp01(speedFraction);
+            _navMeshAgent.isStopped = false;
+        }
+    }
+}
+
+
