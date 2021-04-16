fa817af3e8db035851f144463d733cde4f1a52bf
ivan lashin <iw.lasgin2011@gmail.com>
2021-04-16 03:48:10 +0300

    add restart button

---
+++ Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TestLavaProject.UI
+{
+    public class RestartButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        private void Awake()
+        {
+            _button.onClick.AddListener(OnRestartButton);
+        }
+
+        private void OnRestartButton()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        private void OnDestroy()
+        {
+            _button.onClick.RemoveListener(OnRestartButton);
+        }
+    }
+}
